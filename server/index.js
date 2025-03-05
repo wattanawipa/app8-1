364dcc8cf585986b325dc17a38f25620dee150f8
wattanawipa <minn7714@gmail.com>
2025-03-05 14:37:27 +0700

    commit

---
+++ server/index.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const app = express()
+const port = 8000
+
+
+app.get('/',(request, response) =>{
+    response.send(`
+        <!doctype html>
+        <html>
+        <head>
+            <title>Express</title>
+        </head>
+        <body>
+        <h3>Welcome to Express</h3>
+        <b>Express.js Fast, unopinionated, minimalist <br>
+        web framework for Node.js</b>
+        </body>
+        </html>
+        `)
+})
+app.listen(port, () => console.log('Server listening on port'+port)) 
